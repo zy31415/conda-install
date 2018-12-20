e56ea9f38c1a4e76b0f6b6973c86dfaf40428f89
张杨 <zhangyang.tt@bytedance.com>
2018-12-20 13:28:00 +0800

    Add dummy run.
    Change-Id: I979e827dfbc3c55b7a2396a93a0f1c5ed66eb307

---
+++ conda-install-old.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Help messages:
+usage="$(basename "$0") [-h] -- program to install conda packages based on requirements.txt file.
+
+Use the following commenting line:
+# -c conda-forge
+to ask to install the following package from conda-forge channel.
+
+Arguments:
+    -h  show this help text"
+
+seed=42
+while getopts ':hs:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    s) seed=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# The main program starts from here:
+while read line; do
+
+  # Recognize commenting line:
+  # # -c conda-forge
+  # so that the following package will be installed from the above channel.
+  if [[ ${line} =~ ^\#[[:space:]]+-c ]];then
+    chanel=${line:1}
+
+  # Ignore commenting lines.
+  elif [[ ${line} =~ ^\# ]];then
+    continue
+
+  # Ignore empty lines:
+  elif [[ ${line} =~ [[:space]]* ]];then
+    continue
+
+  # Install a package. Install use -c conda-forge is the variable chanel is defined.
+  else
+    echo "conda install $chanel ${line}"
+    conda install ${chanel} ${line}
+    chanel=""
+  fi
+done <requirements.txt
--- conda-install.sh
+++ conda-install.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Help messages:
+usage="$(basename "$0") [-h] -- program to install conda packages based on requirements.txt file.
+
+Use the following commenting line:
+# conda install xyz
+to run a conda install.
+
+Arguments:
+    -h  show this help text
+    -d  dummy run" 
+
+dummy=false
+
+while getopts ':hd' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    d) dummy=true
+        ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 requirements=$1
+
+# The main program starts from here:
 
 function print_current_conda_env () {
     echo "Your current conda env is:"
@@ -19,9 +57,16 @@ do
     if [[ ${line} =~ ^\#[[:space:]]conda[[:space:]]install ]]
     then 
         cmd=${line:2}
-        echo 
-        echo ">> Installing: ${cmd}"
-        ${cmd}
+
+        if [ "${dummy}" = true ]
+        then
+            echo 
+            echo ">> (Dummy) Installing: ${cmd}"
+        else
+            echo 
+            echo ">> Installing: ${cmd}"
+            ${cmd}
+        fi
     fi
 done < ${requirements}
 
